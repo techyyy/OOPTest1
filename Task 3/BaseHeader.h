03f7e18408e145ebf7efe7a81fcd88e88207becd
techyyy <72086769+techyyy@users.noreply.github.com>
2020-11-01 20:19:49 +0200

    Add files via upload

---
+++ Task 3/BaseHeader.h
@@ -0,0 +1,61 @@
+#ifndef ADDITIONALFUNCS_H
+#define ADDITIONALFUNCS_H
+
+#include<iostream>
+
+double S = 1.0;
+
+class Base{
+    public:
+    Base(){++count;}
+    Base(const Base&){++count;}
+    ~Base() {
+        S = S/2 - 3*count + 29;
+    }
+
+    static size_t howMany()
+    { return count; }
+
+    static size_t count;
+
+};
+
+class Alpha:public Base{
+    public:
+    ~Alpha() { 
+        S = S+count;
+    }
+    private:
+
+    Base obj1, obj2;
+    static size_t count;
+};
+
+class Beta:public Base{
+    public:
+    ~Beta() { 
+        S = S - 2*count - 17; 
+        Base::count--;
+    }
+    private:
+    Base obj1, obj2;
+    static size_t count;
+};
+
+class Gamma:public Base{
+     public:
+    ~Gamma() { 
+        S = S + count - 29;
+    }
+
+    private:
+    Base obj1, obj2;
+    static size_t count;
+};
+
+size_t Base::count = 0;
+size_t Alpha::count = 0;
+size_t Beta::count = 0;
+size_t Gamma::count = 0;
+
+#endif
---
+++ Task 3/main.cpp
@@ -0,0 +1,8 @@
+
+#include<iostream>
+#include"BaseHeader.h"
+
+int main()
+{
+    
+}
